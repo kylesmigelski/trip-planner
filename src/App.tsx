d795ccaec105f62127bb38034927df4fe9abf2fa
MacKayAl56 <mackayal@mail.gvsu.edu>
2023-03-20 23:46:03 -0400

    Designed opening page

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Background from './airplane.jpg';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{  
+      backgroundImage: `url(${Background})`,
+      backgroundPosition: 'center',
+      backgroundSize: 'cover',
+      width: '100vw',
+      height: '100vh',
+      backgroundRepeat: 'no-repeat' 
+    }}>
+      <header className='App-header'>Welcome to Trip Planner</header>
+      <p className='App-text'>Let's plan your dream vacation!
+        <br></br><button className='button'>Get Started</button>
+      </p>
+      
     </div>
   );
 }
