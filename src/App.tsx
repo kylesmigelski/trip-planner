4d1b2303e2ba0e440a21612fc97f8a8d47792a73
MacKayAl56 <mackayal@mail.gvsu.edu>
2023-04-02 23:44:36 -0400

    Connected firebase database

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
 import './App.css';
 import Background from './airplane.jpg';
-
+import  app  from "./firebase.js";
+import { BrowserRouter as Router} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 function App() {
+
+  function Login() {
+    
+  }
+
+  function Signup() {
+    
+  }
+    
+  
+
   return (
     <div style={{  
       backgroundImage: `url(${Background})`,
@@ -15,7 +29,8 @@ function App() {
     }}>
       <header className='App-header'>Welcome to Trip Planner</header>
       <p className='App-text'>Let's plan your dream vacation!
-        <br></br><button className='button'>Get Started</button>
+        <br></br><button className='button' onClick={Login}>Login</button>
+        <br></br><button className='button' onClick={Signup}>Register</button>
       </p>
       
     </div>
