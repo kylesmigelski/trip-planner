4936ea3b3eb07a2d187fafcea53f905431abaafa
Kyle Smigelski <smigelky@mail.gvsu.edu>
2023-04-24 17:03:28 -0400

    fix chart

--- src/components/ratingschart.tsx
+++ src/components/ratingschart.tsx
@@ -1,58 +1,77 @@
 import React from 'react';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend,
+} from 'chart.js';
 import { Bar } from 'react-chartjs-2';
-import {ChartOptions} from "chart.js";
-import { CategoryScale, Chart } from "chart.js";
 
-Chart.register(CategoryScale);
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+);
 
-type RatingsChartProps = {
+type Props = {
     ratings: number[];
 };
 
-const RatingsChart: React.FC<RatingsChartProps> = ({ ratings }) => {
-    const data = {
-        labels: ['1', '2', '3', '4', '5'],
-        datasets: [
-            {
-                label: 'Ratings',
-                data: ratings,
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(255, 159, 64, 0.2)',
-                    'rgba(255, 205, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(255, 159, 64, 1)',
-                    'rgba(255, 205, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(54, 162, 235, 1)',
-                ],
-                borderWidth: 1,
-            },
-        ],
-    };
-
-    const options: ChartOptions = {
-        indexAxis: 'y',
-        scales: {
-            x: {
-                type: 'category',
-                beginAtZero: true,
-            },
-            y: {
-                type: 'linear',
-            },
+export const options = {
+    indexAxis: 'y' as const,
+    elements: {
+        bar: {
+            borderWidth: 2,
         },
-    };
+    },
+    responsive: true,
+    plugins: {
+        legend: {
+            position: 'right' as const,
+        },
+        title: {
+            display: true,
+            text: 'Chart.js Horizontal Bar Chart',
+        },
+    },
+};
 
-    return (
-        <div>
-            <Bar data={data} options={options} />
-        </div>
-    );
+const labels = ['1 Star', '2 Stars', '3 Stars', '4 Stars', '5 Stars'];
+
+export const data = {
+    labels,
+    datasets: [
+        {
+            label: 'Ratings Count',
+            data: [] as number[],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
+                'rgba(255, 205, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(255, 159, 64, 1)',
+                'rgba(255, 205, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(54, 162, 235, 1)',
+            ],
+            borderWidth: 1,
+        },
+    ],
+};
+
+const RatingsChart: React.FC<Props> = ({ ratings }) => {
+    data.datasets[0].data = ratings;
+    return <Bar options={options} data={data} />;
 };
 
 export default RatingsChart;
